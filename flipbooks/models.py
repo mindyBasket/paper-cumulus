fb090d67606ac1fb323882382c2dfec32bb0043a
Decoy J <sandcubed@gmail.com>
2017-08-01 18:42:42 +0000

    Added and tested 3 model chains, Scene-Strip-Frame

--- flipbooks/models.py
+++ flipbooks/models.py
@@ -4,19 +4,57 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
-class Frame(models.Model):
+
+    
+# Scene: holds multiple strips. In convensional web-comic sense, this is like a "page"
+class Scene(models.Model):
+    
+    order = models.IntegerField(default="0") 
+    
+    description = models.TextField(max_length=100, blank=True, default="")
+    
+    #chapter = models.ForeignKey(Chapter, on_delete=models.CASCADE)
+    
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+
+
+# Strip: holds multiple frames. Used for viewing frames.
+class Strip(models.Model):
+    
+    order = models.IntegerField(default="0") 
+    
+    description = models.TextField(max_length=100, blank=True, default="")
+    
+    scene = models.ForeignKey(Scene, on_delete=models.CASCADE)
     
     date_created = models.DateTimeField(auto_now_add=True)
     date_modified = models.DateTimeField(auto_now=True)
     
+    
+#Frame: holds individual frames
+class Frame(models.Model):
+    
+    order = models.IntegerField(default="0") 
+    
     #note = models.CharField(max_length=255, blank=False, unique=True)
-    note = models.TextField(max_length=100, blank=True, default="")
+    note = models.CharField(
+        max_length=100, blank=True, default="", help_text="This note will not be visible for viewers. It's just for the creator"
+    )
+    
+    strip = models.ForeignKey(Strip, blank=True, null=True)
     
     #frame images
     frame_image = models.ImageField(upload_to = 'frame_images/', blank=True)
     
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+    
     
     def __str__(self):
-        #How can I print its own id? self.id?
         return ("%d : %s" % (self.id, self.note))
 
+
+
+    
+    
--- flipbooks/tests.py
+++ flipbooks/tests.py
@@ -3,4 +3,63 @@ from __future__ import unicode_literals
 
 from django.test import TestCase
 
+from .models import (
+    Scene,
+    Strip,
+    Frame
+    )
 # Create your tests here.
+
+# Run using 
+# python manage.py test flipbooks.tests.AnimalTestCase
+
+class ModelTestCase(TestCase):
+    
+    def setUp(self):
+        """Define the test client and other test variables."""
+        self.scene = Scene(order=1, description="Test scene")
+        
+        self.strip = Strip(order=2, description="First strip")
+        self.frame1 = Frame()
+        self.frame2 = Frame()
+        self.frame3 = Frame()
+        
+        #test if it can incrementally add order number later
+    
+    #note: all test case must start with "test_"
+    def test_model_can_create_model_chain(self):
+        """Test the Scene-Strip-Frame model link creation."""
+        old_count = Scene.objects.count()
+        self.scene.save()
+        new_count = Scene.objects.count()
+        
+        self.assertNotEqual(old_count, new_count)
+        
+        #...
+        
+        self.strip.scene=self.scene
+        
+        old_count = Strip.objects.count()
+        self.strip.save()
+        new_count = Strip.objects.count()
+        
+        self.assertNotEqual(old_count, new_count)
+        
+        #...
+        
+        self.frame1.strip=self.strip
+        self.frame2.strip=self.strip
+        self.frame3.strip=self.strip
+        
+        old_count = Frame.objects.count()
+        self.frame1.save()
+        self.frame2.save()
+        self.frame3.save()
+        new_count = Frame.objects.count()
+        
+        self.assertEqual(old_count, new_count-3)
+        
+        
+        
+    # def mytest(self):
+    #     self.assertRaises(FooException, Thing, name='1234')
