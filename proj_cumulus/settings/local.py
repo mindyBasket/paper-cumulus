355a4a405b9303fa6cc13376c6f0ffc5fcff3439
Meehyun Jang <mjang@robomatter.com>
2018-09-26 09:34:38 -0400

    Update syntax style in settings so that Admin site is working again

--- proj_cumulus/settings/local.py
+++ proj_cumulus/settings/local.py
@@ -47,15 +47,14 @@ INSTALLED_APPS = [
     'frontend',   
 ]
 
-MIDDLEWARE_CLASSES = [
+MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
 ROOT_URLCONF = 'proj_cumulus.urls'
