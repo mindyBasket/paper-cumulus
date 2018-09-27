566f64eb5924b43ae8250659ebe51f40b0907d2e
blacksandbox <sandcubed@gmail.com>
2018-09-26 23:09:49 -0400

    Make Curtain component that animates upon clicking DOM outside of the component

--- frontend/src/components/Flipbook.js
+++ frontend/src/components/Flipbook.js
@@ -37,30 +37,85 @@ Table.propTypes = {
 };
 
 
+// Static function 
+// This function is accessed by another element. Triggered externally, but effects the Curtain.
+// https://www.codeproject.com/Tips/1215984/Update-State-of-a-Component-from-Another-in-React
+function updateCurtain(text){
+  this.setState({msg: text});
+  this.expandCurtain();
+}
 
 
 class Curtain extends Component {
   static propTypes = {
     color: PropTypes.string.isRequired,
   };
-  state = {
-      open: "TRUE"
+
+  constructor(props){
+    super(props);
+    this.state = {
+      msg: "...nothing playing",
+      opened: false
+
     };
+    this.node_curtain; //don't define it here, nothing's rendered
+
+    // bind to external function
+    updateCurtain = updateCurtain.bind(this);
+  }
+  
+  findNodes(){
+    // Finds its DOM nodes after it is mounted.
+    // TODO: pretty sure there is a better way than this.
+    //       Investigate how others implement animation with React Comps
+    this.node_curtain = document.querySelector("#curtain");
+    console.log("findNode done");
+  }
+
+  expandCurtain(){
+    // lock scroll
+
+
+    // visibility
+    // ref: https://codepen.io/Shokeen/pen/QgEyLa
+    var curtain = anime({
+      targets: this.node_curtain,
+      scale: 30,
+      easing: 'easeInCubic',
+      duration: 300
+    });
+
+  }
 
   componentDidMount() {
-   
+    this.findNodes();
+
+
+    // need to bind clicks onto the scene buttons. can you do that?
+
+    // TODO: below is bad method to give each scene to call a function upon click. 
+    //       They should really be a react component.
+    const buttonList = document.querySelectorAll('.flipbook-btn');
+
+    for(let i=0; i<buttonList.length; i++){
+      // bind function to each of the button 
+      buttonList[i].onclick = () => {
+        updateCurtain("play " + buttonList[i].getAttribute("id"));
+      };
+
+
+    }
   }
   render() {
     return (
-      <p>{this.state.open}</p>
+      <div>
+        {this.state.msg}
+        <div id="curtain"></div>
+      </div>
+      
     )
   }
 }
-
-
-  
-
-
 
 
 const Flipbook = () => (
@@ -68,7 +123,7 @@ const Flipbook = () => (
 );
 
 // render flipbook
-const wrapper = document.getElementById("flip");
+const wrapper = document.getElementById("flipbook");
 wrapper ? ReactDOM.render(<Flipbook />, wrapper) : null;
 
 
