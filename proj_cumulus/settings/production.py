2e1de882c70ab3ef2e2c62498ba220888fda05f0
blacksandbox <sandcubed@gmail.com>
2018-10-21 22:30:26 -0400

    Testing with a copy of production.py and unignored main.js

--- proj_cumulus/settings/production.py
+++ proj_cumulus/settings/production.py
@@ -1,9 +1,21 @@
 from .base import *
 
-DEBUG = False
+# SECURITY WARNING: keep the secret key used in production secret!
+# SECRET_KEY = '$k3(_qgf48hzeddm(fw-8^aj1pk%u0l%w8sa2&kzpyr$ufjj8@'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+
+# Additional apps for local dev
+# INSTALLED_APPS += []
+#         'debug_toolbar',
+# ]
+
 
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -12,13 +24,13 @@ DATABASES = {
 }
 
 
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
 
 # Storage and serves
-# TODO: make this more appropriate for Production
 
 # If /static/ above doesn't exist, it will look at the following paths.
 # Wait...this IS served directly when I run this app.
@@ -30,6 +42,7 @@ STATICFILES_DIRS = [
 
 #Is served from (or collectStatic'd into)
 STATIC_ROOT = os.path.join(BASE_DIR, "static-serve")
+
 
 # Uploaded Media
 # https://docs.djangoproject.com/en/1.11/howto/static-files/#serving-files-uploaded-by-a-user-during-development
