420c9a19e1e7d3d05b01dd87de8409b98d2dbc72
blacksandbox <sandcubed@gmail.com>
2018-12-03 00:35:45 -0500

    Add STATIC_ROOT for heroku production app

--- proj_cumulus/settings/herokuprod.py
+++ proj_cumulus/settings/herokuprod.py
@@ -64,3 +64,8 @@ MEDIAFILES_LOCATION = 'media'
 MEDIA_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, MEDIAFILES_LOCATION)
 DEFAULT_FILE_STORAGE = 'proj_cumulus.settings.media_backends.MediaStorage'
 THUMBNAIL_DEFAULT_STORAGE = 'proj_cumulus.settings.media_backends.MediaStorage'
+
+
+# Map static
+# ref: https://stackoverflow.com/questions/30624104/django-on-amazon-web-service-aws
+STATIC_ROOT = os.path.join(BASE_DIR, '..','static')
