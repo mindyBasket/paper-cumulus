4e48227e42edaee0d960786a32d7879108136a86
Mindy Jang <sandcubed@gmail.com>
2020-08-30 23:45:21 -0400

    Unrelay queries and mutations

--- storypiper/schema/schema.py
+++ storypiper/schema/schema.py
@@ -1,152 +1,210 @@
-import json
 import graphene
-from graphene import relay, ObjectType
 from graphene_django import DjangoObjectType
-from graphene_django.filter import DjangoFilterConnectionField
 from graphql import GraphQLError
 
 from django.db.models import Q
 from .. import models
 
-# Note: if you add new object here, make sure you update the "entry point" in
-#       proj_cumulus/schema.py
+# MAINTANENCE NOTE:
+# if you add new object here, make sure you update the "entry point" in
+# proj_cumulus/schema.py
 
-# Interfaces
-# TODO: learn about Interfaces here: https://docs.graphene-python.org/en/latest/types/interfaces/
-# This a reminder for you add interface later for sake of practice
+# Interfaces Ref: https://docs.graphene-python.org/en/latest/types/interfaces/
+class DefaultInterface(graphene.Interface):
+    pk = graphene.ID(required=True)
+    title = graphene.String(required=False)
 
-# Graphene will automatically map the Category model's fields onto the CategoryNode.
+# NOTE: Graphene will automatically map the Category model's fields onto the CategoryNode.
 # This is configured in the CategoryNode's Meta class (as you can see below)
 
-# More about ObjectType meta fields: https://docs.graphene-python.org/projects/django/en/latest/queries/
-class SeriesNode(DjangoObjectType):
+# More about DjangoObjectType meta fields and filtering: 
+# https://docs.graphene-python.org/projects/django/en/latest/queries/
+# https://docs.graphene-python.org/projects/django/en/latest/filtering/
+
+class SeriesType(DjangoObjectType):
     class Meta:
         model = models.Series
-        filter_fields = ['title']
-        interfaces = (relay.Node, )
-    
-    pk = graphene.Int(source='pk')
+        interfaces = (DefaultInterface,)
 
-class FlipbookNode(DjangoObjectType):
+class FlipbookType(DjangoObjectType):
     class Meta:
         model = models.Flipbook
-        # Allow for some more advanced filtering here
-        filter_fields = {
-            'id64': ['exact'],
-            'title': ['exact', 'icontains', 'istartswith'],
-            'description': ['exact', 'icontains'],
-            'series__title': ['exact', 'icontains'],
-        }
-        interfaces = (relay.Node,)
+        fields = ('pk', 'id64', 'description', 'series',)
+        interfaces = (DefaultInterface, )
+    
+    # pk = graphene.Int(source='pk') # extra custom field
 
-    pk = graphene.Int(source='pk')
+    # NOTE: define a custom get_queryset method to control filtering on the ObjectType level
+    #       instead of the Query object level.
+    #       ref: https://docs.graphene-python.org/projects/django/en/latest/queries/#default-queryset
+
+    # @classmethod
+    # def get_queryset(cls, queryset, info):
+    #     if info.context.user.is_anonymous:
+    #         return queryset.filter(published=True)
+    #     return queryset
+
+# ref: https://docs.graphene-python.org/en/latest/types/objecttypes/#default-resolver
+class DeletedFlipbookType(graphene.ObjectType):
+    title = graphene.String()
+    # last_name = String()
+
+    @staticmethod
+    def resolve_title(parent, info):
+        # Arg to this resolve will be a DICT.
+        # Dict is not an object in Python. Don't access values using dot operators
+        return parent['title']
+
 
 # Resolvers
 class Query(graphene.ObjectType):
-    flipbook = relay.Node.Field(FlipbookNode)
-    all_flipbooks = DjangoFilterConnectionField(FlipbookNode)
+    all_flipbooks = graphene.List(
+        FlipbookType,
+        search=graphene.String(),
+        first=graphene.Int(),
+        skip=graphene.Int(),
+    )
 
-    series = relay.Node.Field(SeriesNode)
-    all_seriess = DjangoFilterConnectionField(SeriesNode)
+    all_flipbooks_by_title = graphene.List(
+        FlipbookType,
+        title=graphene.String(),
+    )
 
-class CreateFlipbook(graphene.relay.ClientIDMutation):
-    # Similar to "Argument", but you can pass a whole object into Input.
-    # Note that "Argument" is depreciated in Graphene 2.0
-    class Input:
+    flipbook = graphene.Field(
+        FlipbookType,
+        id=graphene.Int(),
+        id64=graphene.String(),
+    )
+
+    seriess = graphene.List(SeriesType)
+
+    @staticmethod
+    def resolve_all_flipbooks(parent, info, search=None, first=None, skip=None, **kwargs):
+        flipbooks_queryset = models.Flipbook.objects.all()
+
+        if search:
+            filter = (
+                Q(title__icontains=search) |
+                Q(description__icontains=search) |
+                Q(series__title__icontains=search)
+            )
+            
+            return flipbooks_queryset.filter(filter)
+        
+        if skip:
+            flipbooks_queryset = flipbooks_queryset[skip:]
+
+        if first:
+            flipbooks_queryset = flipbooks_queryset[:first]
+
+        return flipbooks_queryset
+    
+    @staticmethod
+    def resolve_all_flipbooks_by_title(parent, info, title):
+        return models.Flipbook.objects.filter(title__icontains=title)
+    
+    @staticmethod
+    def resolve_flipbook(parent, info, **kwargs):
+        # individual query
+        pk = kwargs.get('id')
+        id64 = kwargs.get('id64')
+
+        if pk is not None:
+            return models.Flipbook.objects.get(pk=pk)
+
+        if id64 is not None:
+            return models.Flipbook.objects.get(id64=id64)
+        
+        return None
+
+    @staticmethod
+    def resolve_seriess(parent, info, **kwargs):
+        return models.Series.objects.all()
+
+
+
+class CreateFlipbook(graphene.Mutation):
+    class Arguments:
         title = graphene.String()
         description = graphene.String() # there has to be a way to make this optional
         series_id = graphene.Int()
 
-    @classmethod
-    def mutate_and_get_payload(cls, root, info, **input):
-        # user = info.context.user or None
+    def mutate(self, info, title, description="", series_id=None):
         series = None
-        series_id = input.get('series_id')
         if series_id:
             series = models.Series.objects.filter(id=series_id).first()
             if not series:
                 raise Exception('Cannot find series with id {}!'.format(series_id))
 
-        flipbook = models.Flipbook(
-            title=input.get('title'),
-            description=input.get('description'),
-            series=series
+        new_flipbook = models.Flipbook(title=title, description=description, series=series)
+        new_flipbook.save()
+
+        return CreateFlipbook(
+            success=True,
+            flipbook=new_flipbook
         )
-        flipbook.save()
-
-        return CreateFlipbook(flipbook=flipbook)
     
-    # output type
-    flipbook = graphene.Field(FlipbookNode) 
+    # Outputs: match with mutate payload fields above
+    success = graphene.Boolean()
+    flipbook = graphene.Field(FlipbookType)
 
-class UpdateFlipbook(graphene.relay.ClientIDMutation):
-    class Input:
+class UpdateFlipbook(graphene.Mutation):
+    class Arguments:
         pk = graphene.Int(required=True)
-
         title = graphene.String()
         description = graphene.String()
+        series_id = graphene.Int()
 
     @classmethod
-    def mutate_and_get_payload(cls, root, info, **input):
-        # grab the flipbook?
+    def mutate(cls, root, info, **input):
         pk = input.get('pk')
         updated_flipbook = models.Flipbook.objects.filter(pk=pk).first()
 
         if updated_flipbook:
             for key, value in input.items():
                 if (key == 'title') and not value:
-                    raise Exception('A flipbook cannot have an empty title!')
+                    raise GraphQLError('A flipbook cannot have an empty title!')
                 else:
                     setattr(updated_flipbook, key, value)
         else:
-            raise Exception('Could not find Flipbook to edit!')
+            raise GraphQLError('Could not find Flipbook to edit!')
 
         updated_flipbook.save()
-        return UpdateFlipbook(flipbook=updated_flipbook)
-
-    # output type
-    flipbook = graphene.Field(FlipbookNode)
-
-# TODO: eventually I would like to add an "archive" behavior, so that you can
-#       'undo' deletion.
-
-# Learn more about json dumps: https://docs.python.org/3/library/json.html
-class DeleteFlipbook(graphene.relay.ClientIDMutation):
-    # output type
-    response = graphene.String()
-
-    class Input:
-        gid = graphene.ID(required=True)
-
-    @classmethod
-    def mutate_and_get_payload(cls, root, info, **input):
-        # pk = relay.Node.from_global_id(input.get('id'))[1]
-        # target_flipbook = models.Flipbook.objects.get(pk=pk)
-        target_flipbook = relay.Node.get_node_from_global_id(
-            info,
-            input.get('gid'),
-            only_type=FlipbookNode
+        return cls(
+            success=True,
+            flipbook=updated_flipbook
         )
 
+    # Outputs: match with mutate payload fields above
+    success = graphene.Boolean()
+    flipbook = graphene.Field(FlipbookType)
+
+class DeleteFlipbook(graphene.Mutation):
+    class Arguments:
+        pk = graphene.Int(required=True)
+
+    @classmethod
+    def mutate(cls, root, info, **input):
+        pk = input.get('pk')
+        target_flipbook = models.Flipbook.objects.filter(pk=pk).first()
+
         if target_flipbook:
-
             object_deleted = {
-                'id64': target_flipbook.id64,
                 'title': target_flipbook.title
-            }
-            if target_flipbook.series:
-                object_deleted['series__title'] = target_flipbook.series.title
-
-            response_json = {
-                'success': True,
-                'object_deleted': object_deleted
             }
 
             target_flipbook.delete()
-            return cls(response=json.dumps(response_json))
-
+            print("Object deleted!")
+            return cls(
+                success=True,
+                flipbook=object_deleted
+            )
         else:
-            raise Exception('Could not find Flipbook to delete!')
+            raise GraphQLError('Could not find Flipbook to delete!') # can also use Exception()
+        
+
+    success = graphene.Boolean()
+    flipbook = graphene.Field(DeletedFlipbookType)
 
 class Mutation(graphene.ObjectType):
     create_flipbook = CreateFlipbook.Field()
